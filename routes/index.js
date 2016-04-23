5fabde12f209564e1f66ce6477bd580502aff8b5
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2016-04-23 22:43:36 +0200

    Don't use the layout

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,11 @@ router.get('/', function(req, res, next) {
 
 var shoppingCart = [];
 router.get('/shopping-cart', function(req, res, next) {
-  res.render('shopping-cart', { shoppingCartLength: shoppingCart.length });
+  res.render('shopping-cart', { shoppingCartLength: shoppingCart.length, layout: false });
 });
 
 router.get('/shopping-cart-full', function(req, res, next) {
-  res.render('shopping-cart-full', { shoppingCartLength: shoppingCart.length });
+  res.render('shopping-cart-full', { shoppingCartLength: shoppingCart.length, layout: false });
 });
 
 router.post('/', function(req, res, next) {
