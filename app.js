68fe282b867cecfd9c66ec89b19340829069d0a8
Gustaf Nilsson Kotte <gustaf.nk@jayway.com>
2016-04-24 21:59:07 +0200

    Add cors support

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -8,6 +9,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 
 var app = express();
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
